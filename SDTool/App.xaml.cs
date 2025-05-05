e61e34ca315ff068ecc4715d2e2274cc057c6e10
tonyCYuan <145431892+tonyCYuan@users.noreply.github.com>
2025-05-06 00:35:03 +0200

    Add App.xaml.cs

---
+++ SDTool/App.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace SDTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Application initialization code can go here
+        }
+    }
+}
